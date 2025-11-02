66ccbee6c95ed469aa308826950ea215aa7b54f1
angelnyambi <angel_nyambi@outlook.com>
2025-11-02 13:39:10 +0200

    import file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +25,9 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+      </div>
+      <div>
+        <WelcomeMessage />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
