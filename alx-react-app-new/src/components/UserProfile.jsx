16d02e16150f3b1337082f6143951384a61c146b
angelnyambi <angel_nyambi@outlook.com>
2025-11-09 20:30:19 +0200

    add css userprofile

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 const UserProfile = (props) => {
     return (
-        <div>
+        <div style= {{ border: '1px solid blue', padding: '10px', margin: '10px' }}>
             <h2 style={{ color: 'red'}}>{props.name}</h2>
             <p>Age: <span style={{ textAlign= 'right' color: 'white'  }}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
